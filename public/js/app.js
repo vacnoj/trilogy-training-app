555ea00bf9a42900bde45886925663ed5af77c8c
cphelps987 <cphelps987@gmail.com>
2018-09-20 22:22:32 -0400

    updated all contnent pages with the new css classes - also flipped them

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,14 @@
-console.log('app.js loaded')
+$(document).ready(function() {
+
+    console.log('app.js loaded');
+
+    // When the user scrolls the page, execute myFunction
+    document.onscroll = function() {myFunction()};
+
+    function myFunction() {
+        let winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+        let height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+        let scrolled = (winScroll / height) * 100;
+        document.getElementById("myBar").style.width = scrolled + "%";
+    }
+});
